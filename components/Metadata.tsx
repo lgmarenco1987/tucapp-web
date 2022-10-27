bba6b9ac0afc3fe25364b477da66812c411be05e
Luis Rodriguez <lgrodriguez1987@gmail.com>
2022-10-27 16:16:23 -0500

    Metadata config fix

--- components/Metadata.tsx
+++ components/Metadata.tsx
@@ -1,25 +1,32 @@
 import Head from "next/head"
 
 export default function Metadata({ title, descr = null, img = null }) {
+    //<meta property="og:url" content={ogUrl} key="ogurl" />
     return (
         <Head>
+                  
+      
+    
+      
             <title>{title}</title>
+            <meta property="og:title" content={title} key="ogtitle" />
             {descr &&
                 <>
                     <meta name="description" content={descr} key="desc" />
                     <meta
                         property="og:description"
                         content={descr}
+                        key="ogdesc"
                     />
                 </>
 
             }
             {img &&
                 <>
-                    <meta property="og:title" content={title} />
                     <meta
                         property="og:image"
                         content={img}
+                        key="ogimage"
                     />
                 </>
             }
