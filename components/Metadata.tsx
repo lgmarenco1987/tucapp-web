672217fbccac3e3517db9603bf3fd5a67d4c78b9
Luis Rodriguez <lgrodriguez1987@gmail.com>
2022-10-27 16:25:07 -0500

    Remove unecessary tags and condition og

--- components/Metadata.tsx
+++ components/Metadata.tsx
@@ -4,35 +4,10 @@ export default function Metadata({ title, descr = null, img = null }) {
     //<meta property="og:url" content={ogUrl} key="ogurl" />
     return (
         <Head>
-                  
-      
-    
-      
             <title>{title}</title>
-            <meta property="og:title" content={title} key="ogtitle" />
-            <meta property="og:url" content="https://tucapp.com" key="ogurl" />
-            <meta property="og:site_name" content="Capp tu Aliado en los negocios" key="ogsitename" />
-            <meta property="og:description" content="Capp tu Aliado en los negocios" key="ogdesc" />
-            {descr &&
-                <>
-                    <meta name="description" content={descr} key="desc" />
-                    <meta
-                        property="og:description"
-                        content={descr}
-                        key="ogdesc"
-                    />
-                </>
-
-            }
-            {img &&
-                <>
-                    <meta
-                        property="og:image"
-                        content={img}
-                        key="ogimage"
-                    />
-                </>
-            }
+            <meta property="og:title" content={title}/>
+            {descr && <meta property="og:description" content={descr}/>}
+            {img && <meta property="og:image" content={img}/>}
         </Head>
     )
 }
